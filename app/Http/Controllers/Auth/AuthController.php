170530df78762dd30a16bf4e653c98ef87c38fe2
Jason McCreary <jason@pureconcepts.net>
2015-05-13 21:28:28 -0400

    Updated Auth default redirect

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    protected $redirectTo = '/';
 
     /*
 	|--------------------------------------------------------------------------
