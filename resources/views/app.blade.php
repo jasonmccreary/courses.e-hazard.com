488fc01c659757a931cd61db5429951a87a9b831
Jason McCreary <jason@pureconcepts.net>
2015-05-21 09:57:32 -0400

    Changed logo name

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Courses Administration</title>
+	<title>Courses Management System</title>
 
     <link href="{{ asset('/css/bootstrap-datepicker3.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
